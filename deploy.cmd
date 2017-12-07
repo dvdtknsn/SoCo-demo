e9f66d4f3e2cdee3c82d4aaa5817205ee247e57e
David Atkinson <dvdtknsn@gmail.com>
2017-12-07 20:44:10 +0000

    Got my scripts folder wrong

--- deploy.cmd
+++ deploy.cmd
@@ -1,1 +1,2 @@
-Tools\sco.exe /source Doerte_dev{DOERTE_DEV} /target DOERTE_PROD/demopassword@localhost/XE{DOERTE_PROD} /deployallobjects /sf:state_deploymentscript.sql
+Tools\sco.exe /source Scriptsfolder{DOERTE_DEV} /target DOERTE_PROD/demopassword@localhost/XE{DOERTE_PROD} /deployallobjects /sf:state_deploymentscript.sql
+
