af627031225d51095e624f964f6dcaa71d70d88c
David Atkinson <dvdtknsn@gmail.com>
2017-12-07 20:47:59 +0000

    Adding error 61 error handling
    we need the batch file to retrun 0

--- deploy.cmd
+++ deploy.cmd
@@ -1,2 +1,23 @@
+
+
 Tools\sco.exe /source Scriptsfolder{DOERTE_DEV} /target DOERTE_PROD/demopassword@localhost/XE{DOERTE_PROD} /deployallobjects /sf:state_deploymentscript.sql
+
+echo %ERRORLEVEL%
+rem IF ERRORLEVEL is 61 then all is good as we expect changes.
+IF %ERRORLEVEL% EQU 61 (
+    set ERRORLEVEL=0
+    GOTO END
+)
+
+rem IF ERRORLEVEL is 0 then it's gone wrong so set errorlevel to make build step fail
+IF %ERRORLEVEL% EQU 0 (
+    echo ========================================================================================================
+    echo == No changes found to deploy !!!!  Aborting build...
+    echo ========================================================================================================
+    set ERRORLEVEL=1
+)
+
+:END
+EXIT /B %ERRORLEVEL%
+
 
