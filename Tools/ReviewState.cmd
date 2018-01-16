56e5201754f3f6ba75de6b08edf58bd566f89194
David Atkinson <dvdtknsn@gmail.com>
2018-01-16 14:05:52 +0000

    fixed where deployemtn script is generated

--- Tools/ReviewState.cmd
+++ Tools/ReviewState.cmd
@@ -8,8 +8,6 @@ rem If the exit code is 63, the deployment warnings are above the allowable thre
 rem This means that it is recommended to do a manual deployment, or consider adopting a migrations-based deployment approach
 
 IF %ERRORLEVEL% EQU 63 (
-    rem We have to run the same comparison to generate the deployment script as the warnings will abort the generation of the deployment script
-    "C:\Program Files\Red Gate\Schema Compare for Oracle 4\sco.exe" /i:sdwgvac /source State{SOCO_DEV} /target SOCO_PRODUCTION/demopassword@localhost/XE{SOCO_PRODUCTION} /sf:Artifacts/deployment_preview.sql
     echo ========================================================================================================
     echo == High Severity Warnings Detected! Aborting the build. 
     echo == Review the deployment script and consider deploying manually, or adopting a migrations-based deployment approach.
@@ -17,5 +15,9 @@ IF %ERRORLEVEL% EQU 63 (
     GOTO END
 )
 
+rem If there are no warnings we run the same comparison to generate the deployment script (as warnings abort the generation of the deployment script)
+IF %ERRORLEVEL% EQU 0 (
+    "C:\Program Files\Red Gate\Schema Compare for Oracle 4\sco.exe" /i:sdwgvac /source State{SOCO_DEV} /target SOCO_PRODUCTION/demopassword@localhost/XE{SOCO_PRODUCTION} /sf:Artifacts/deployment_preview.sql
+)
 
 :END
