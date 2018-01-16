36b25925872a098651d8dea02df1f0439a16fa67
David Atkinson <dvdtknsn@gmail.com>
2018-01-16 15:57:21 +0000

    wrong exit code

--- Tools/ReviewState.cmd
+++ Tools/ReviewState.cmd
@@ -1,10 +1,11 @@
 echo off
-rem We can generate the deployment preview script artifact here
+
+echo ==  We generate the deployment preview script artifact here (if there are changes) ==
 
 "C:\Program Files\Red Gate\Schema Compare for Oracle 4\sco.exe" /abortonwarnings:high /i:sdwgvac /source State{SOCO_DEV} /target SOCO_PRODUCTION/demopassword@localhost/XE{SOCO_PRODUCTION} /sf:Artifacts/deployment_preview.sql > Artifacts\Warnings.txt
 
 echo Warnings exit code:%ERRORLEVEL%
-rem If the exit code is 63, the deployment warnings are above the allowable threshold
+rem If the exit code is 63, the deployment warnings exceed the allowable threshold
 rem This means that it is recommended to do a manual deployment, or consider adopting a migrations-based deployment approach
 
 IF %ERRORLEVEL% EQU 63 (
@@ -16,8 +17,20 @@ IF %ERRORLEVEL% EQU 63 (
 )
 
 rem If there are no warnings we run the same comparison to generate the deployment script (as warnings abort the generation of the deployment script)
-IF %ERRORLEVEL% EQU 0 (
+IF %ERRORLEVEL% EQU 61 (
+    echo ========================================================================================================
+    echo == Schema changes found to deploy - generating deployment script for review
+    echo ========================================================================================================
     "C:\Program Files\Red Gate\Schema Compare for Oracle 4\sco.exe" /i:sdwgvac /source State{SOCO_DEV} /target SOCO_PRODUCTION/demopassword@localhost/XE{SOCO_PRODUCTION} /sf:Artifacts/deployment_preview.sql
+    GOTO END
+)
+
+IF %ERRORLEVEL% EQU 0 (
+    echo ========================================================================================================
+    echo == No schema changes to deploy
+    echo ========================================================================================================
+    rem If required, halt the build process at this stage
+    GOTO END
 )
 
 :END
