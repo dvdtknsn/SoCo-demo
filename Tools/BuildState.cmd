fa483a03b155c6aed84bc40bb878e0d1e785d43b
David Atkinson <dvdtknsn@gmail.com>
2018-01-16 12:26:38 +0000

    Changing name of report

--- Tools/BuildState.cmd
+++ Tools/BuildState.cmd
@@ -1,7 +1,7 @@
 echo off
 rem Build is a useful test as it can fail if there are invalid objects
 
-"C:\Program Files\Red Gate\Schema Compare for Oracle 4\sco.exe" /i:sdwgvac /source State{SOCO_DEV} /target SOCO_TEST/demopassword@localhost/XE{SOCO_TEST} /report:Artifacts/changes_report.html
+"C:\Program Files\Red Gate\Schema Compare for Oracle 4\sco.exe" /i:sdwgvac /source State{SOCO_DEV} /target SOCO_TEST/demopassword@localhost/XE{SOCO_TEST} /report:Artifacts/all_objects_report.html
 
 echo %ERRORLEVEL%
 
@@ -16,7 +16,7 @@ IF %ERRORLEVEL% EQU 0 (
 rem IF ERRORLEVEL is 61 we set to 0 as we expect differences
 IF %ERRORLEVEL% EQU 61 (
     echo ========================================================================================================
-    echo == Change report changes_report.html saved as an artifact
+    echo == Change report all_objects_report.html saved as an artifact
     echo ========================================================================================================
     rem as wee expect differences we reset the ERRORLEVEL to 0 so the build doesn't fail 
     SET ERRORLEVEL=0
