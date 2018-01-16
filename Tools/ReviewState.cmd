8826a0e772754fd34dc837ebc592ce0be72724ab
David Atkinson <dvdtknsn@gmail.com>
2018-01-16 13:48:06 +0000

    wrong DB

--- Tools/ReviewState.cmd
+++ Tools/ReviewState.cmd
@@ -1,7 +1,7 @@
 echo off
 rem We can generate the deployment preview script artifact here
 
-"C:\Program Files\Red Gate\Schema Compare for Oracle 4\sco.exe" /abortonwarnings:high /i:sdwgvac /source State{REDGATE_DEV} /target REDGATE_PRODUCTION/demopassword@localhost/XE{REDGATE_PRODUCTION} /sf:Artifacts/deployment_preview.sql > Artifacts\Warnings.txt
+"C:\Program Files\Red Gate\Schema Compare for Oracle 4\sco.exe" /abortonwarnings:high /i:sdwgvac /source State{SOCO_DEV} /target SOCO_PRODUCTION/demopassword@localhost/XE{SOCO_PRODUCTION} /sf:Artifacts/deployment_preview.sql > Artifacts\Warnings.txt
 
 echo Warnings exit code:%ERRORLEVEL%
 rem If the exit code is 63, the deployment warnings are above the allowable threshold
@@ -9,7 +9,7 @@ rem This means that it is recommended to do a manual deployment, or consider ado
 
 IF %ERRORLEVEL% EQU 63 (
     rem We have to run the same comparison to generate the deployment script as the warnings will abort the generation of the deployment script
-    "C:\Program Files\Red Gate\Schema Compare for Oracle 4\sco.exe" /i:sdwgvac /source State{REDGATE_DEV} /target REDGATE_PRODUCTION/demopassword@localhost/XE{REDGATE_PRODUCTION} /sf:Artifacts/deployment_preview.sql > Warnings.txt
+    "C:\Program Files\Red Gate\Schema Compare for Oracle 4\sco.exe" /i:sdwgvac /source State{SOCO_DEV} /target SOCO_PRODUCTION/demopassword@localhost/XE{SOCO_PRODUCTION} /sf:Artifacts/deployment_preview.sql
     echo ========================================================================================================
     echo == High Severity Warnings Detected! Aborting the build. 
     echo == Review the deployment script and consider deploying manually, or adopting a migrations-based deployment approach.
