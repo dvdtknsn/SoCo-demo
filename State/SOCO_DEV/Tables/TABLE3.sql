b94a18f0382385b47ca3eb709822e026d0ab643c
dvdtknsn <David.Atkinson@red-gate.com>
2018-02-09 11:51:03 +0000

    New table 3

---
+++ State/SOCO_DEV/Tables/TABLE3.sql
@@ -0,0 +1,7 @@
+CREATE TABLE soco_dev.table3 (
+  column1 VARCHAR2(20 BYTE),
+  column2 VARCHAR2(20 BYTE),
+  column3 VARCHAR2(20 BYTE),
+  column4 VARCHAR2(20 BYTE),
+  column5 VARCHAR2(20 BYTE)
+);
