35c1da66d5f236f3bfdb7a219e53fd7ffda44c1d
David Atkinson <dvdtknsn@gmail.com>
2018-02-14 16:05:29 +0000

    don't need the last sco call?

--- Tools/Release-Review.cmd
+++ Tools/Release-Review.cmd
@@ -1,19 +1,18 @@
 echo off
+rem  We generate the deployment preview script artifact here
 
-echo ==  We generate the deployment preview script artifact here (if there are changes) ==
-
-rem exclude target schema in the scripts using /b:e
-"C:\Program Files\Red Gate\Schema Compare for Oracle 4\sco.exe" /abortonwarnings:high /b:hdre /i:sdwgvac /source State{SOCO_DEV} /target SOCO_PRODUCTION/demopassword@localhost/XE{SOCO_PRODUCTION} /report:Artifacts/changes_report.html /sf:Artifacts/deployment_script.sql > Artifacts\Warnings.txt
+"C:\Program Files\Red Gate\Schema Compare for Oracle 4\sco.exe" /abortonwarnings:high /b:hdre /i:sdwgvac /source State{SOCO_DEV} /target SOCO_PRODUCTION/demopassword@localhost/XE{SOCO_PRODUCTION} /report:Artifacts/changes_report.html /scriptfile:Artifacts/deployment_script.sql > Artifacts\Warnings.txt
+rem Note that for this demo example /b:e is used to exclude the target schema in the deployment script
 
 echo Warnings exit code:%ERRORLEVEL%
-rem If the exit code is 63, the deployment warnings exceed the allowable threshold (eg, data loos may occur)
-rem This means that it is recommended to review, customize the script and do a manual deployment
+rem In the unlikely event that the exit code is 63, this mean that a deployment warning has exceeded the allowable threshold (eg, data loss may have been detected)
+rem If this occurs it is recommended to review the script, customize it, and perform a manual deployment
 
 IF %ERRORLEVEL% EQU 0 (
     echo ========================================================================================================
     echo == No schema changes to deploy
     echo ========================================================================================================
-    rem If desirable we halt the build process at this stage - in Jenkins can set to "unstable"
+    rem If desirable we halt the build process at this stage.
     SET ERRORLEVEL=1
     GOTO END
 )
@@ -21,23 +20,21 @@ IF %ERRORLEVEL% EQU 0 (
 IF %ERRORLEVEL% EQU 63 (
     echo ========================================================================================================
     echo == High Severity Warnings Detected! Aborting the build. 
-    echo == Review the deployment script and consider deploying manually, or adopting a migrations-based deployment approach.
+    echo == Review the deployment script and consider deploying manually.
     echo ========================================================================================================
-    rem We need to run the same comparison without /abortonwarnings to generate the deployment script (as warnings abort the generation of the deployment script)
-    rem It is still useful to have the deployment script artifact for troublshooting purposes, or as a starting point for a manual deployment
-    rem We exclude target schema in the scripts using /b:e
+    rem Here we run the same comparison without /abortonwarnings to generate the deployment script as warnings abort the generation of the deployment script
+    rem It is useful to keep the deployment script artifact for troublshooting purposes, or as the starting point for a manual deployment
     "C:\Program Files\Red Gate\Schema Compare for Oracle 4\sco.exe" /b:hdre /i:sdwgvac /source State{SOCO_DEV} /target SOCO_PRODUCTION/demopassword@localhost/XE{SOCO_PRODUCTION} /report:Artifacts/changes_report.html /sf:Artifacts/deployment_script.sql
     GOTO END
 )
 
+rem This is the happy path where we've identified changes and not detected any high warnings
 IF %ERRORLEVEL% EQU 61 (
     echo ========================================================================================================
     echo == Schema changes found to deploy - generating deployment script for review
     echo ========================================================================================================
     SET ERRORLEVEL=0
-    rem We need to run the same comparison without /abortonwarnings to generate the deployment script (as warnings abort the generation of the deployment script)
-    rem We exclude target schema in the scripts using /b:e
-    "C:\Program Files\Red Gate\Schema Compare for Oracle 4\sco.exe" /b:hdre /i:sdwgvac /source State{SOCO_DEV} /target SOCO_PRODUCTION/demopassword@localhost/XE{SOCO_PRODUCTION} /report:Artifacts/changes_report.html /sf:Artifacts/deployment_script.sql
+    rem "C:\Program Files\Red Gate\Schema Compare for Oracle 4\sco.exe" /b:hdre /i:sdwgvac /source State{SOCO_DEV} /target SOCO_PRODUCTION/demopassword@localhost/XE{SOCO_PRODUCTION} /report:Artifacts/changes_report.html /sf:Artifacts/deployment_script.sql
     GOTO END
 )
 
