54a8906ea8e57d31149ee49b70a76b990bba32cd
dvdtknsn <David.Atkinson@red-gate.com>
2018-02-22 17:53:00 +0000

    Correcting failing tests and adding potentially invalid object

--- State/SOCO_DEV/PackageBodies/TEST_BL_USER_REGISTRATION.sql
+++ State/SOCO_DEV/PackageBodies/TEST_BL_USER_REGISTRATION.sql
@@ -7,12 +7,12 @@ CREATE OR REPLACE package body test_bl_user_registration as
 
     procedure validate_password_strength_2 as
     begin    
-        ut.expect(bl_user_registration.validate_password_strength('%a1B2CD'), '%a1B2CD is a strong password').to_(equal(false));
+        ut.expect(bl_user_registration.validate_password_strength('%a1B2CD'), '%a1B2CD is a strong password').to_(equal(true));
     end validate_password_strength_2;
 
     procedure validate_password_strength_3 as
     begin
-        ut.expect(bl_user_registration.validate_password_strength('%a1B2CD'), '%a1B2CD is a stronger password').to_(equal(false));
+        ut.expect(bl_user_registration.validate_password_strength('%a1B2CD'), '%a1B2CD is a stronger password').to_(equal(true));
     end validate_password_strength_3;
 
 end test_bl_user_registration;
---
+++ State/SOCO_DEV/Packages/UT_STR.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE package ut_str as
+
+  procedure ut_betwn;
+  procedure ut_setup;
+  procedure ut_teardown;
+
+end ut_str;
+/
