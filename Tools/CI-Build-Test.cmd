bd246c502a75aacc2c22919f5edebfdc0be7e268
David Atkinson <dvdtknsn@gmail.com>
2018-02-22 13:42:39 +0000

    using test db for unit tests

--- Tools/CI-Build-Test.cmd
+++ Tools/CI-Build-Test.cmd
@@ -77,7 +77,7 @@ for /f %%A in ('find /c "Invalid Object" ^< Artifacts/invalid_objects.txt') do (
 
 rem Run some unit tests
 echo == Run unit tests ==
-call Tools\utPLSQL-cli\bin\utplsql run SOCO_DEV/demopassword@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=localhost)(Port=1521))(CONNECT_DATA=(SID=XE))) -f=ut_xunit_reporter -o=Artifacts/test_results.xml
+call Tools\utPLSQL-cli\bin\utplsql run SOCO_TEST/demopassword@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=localhost)(Port=1521))(CONNECT_DATA=(SID=XE))) -f=ut_xunit_reporter -o=Artifacts/test_results.xml
 echo utplsql:%ERRORLEVEL%
 
 :END
