b7a0db953ec4f0939f26bf61ccafae3e1ec9884a
David Atkinson <dvdtknsn@gmail.com>
2018-02-25 16:27:18 +0000

    table renames

--- State/SOCO_DEV/Tables/TABLE1.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE table1 (
-  column1 VARCHAR2(20 BYTE),
-  column2 VARCHAR2(20 BYTE),
-  column3 VARCHAR2(20 BYTE)
-);
--- State/SOCO_DEV/Tables/TABLE3.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE soco_dev.table3 (
-  column1 VARCHAR2(20 BYTE),
-  column2 VARCHAR2(20 BYTE),
-  column3 VARCHAR2(20 BYTE),
-  column4 VARCHAR2(20 BYTE),
-  column5 VARCHAR2(20 BYTE)
-);
