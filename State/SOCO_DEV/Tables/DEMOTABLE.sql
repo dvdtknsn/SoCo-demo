b50fd26a0ad35b1875bc943b722d08c230247dc0
dvdtknsn <David.Atkinson@red-gate.com>
2018-02-25 16:43:53 +0000

    some changes

--- State/SOCO_DEV/Tables/DEMOTABLE.sql
+++ State/SOCO_DEV/Tables/DEMOTABLE.sql
@@ -1,5 +1,6 @@
-CREATE TABLE soco_dev.demotable (
+CREATE TABLE demotable (
   column1 VARCHAR2(20 BYTE),
   column2 VARCHAR2(20 BYTE),
-  column3 VARCHAR2(20 BYTE)
+  column3 VARCHAR2(20 BYTE),
+  column4 VARCHAR2(20 BYTE)
 );
