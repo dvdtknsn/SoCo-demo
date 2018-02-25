04085ede4e5b68c68787a3cee4fbcd713494b4e1
dvdtknsn <David.Atkinson@red-gate.com>
2018-02-25 16:50:58 +0000

    fixing unit test

--- State/SOCO_DEV/PackageBodies/TEST_BETWNSTR.sql
+++ State/SOCO_DEV/PackageBodies/TEST_BETWNSTR.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE package body test_betwnstr as
 
   procedure basic_usage is
   begin
-    ut.expect( betwnstr( '1234567', 2, 5 ) ).to_equal('2345davidwashere');
+    ut.expect( betwnstr( '1234567', 2, 5 ) ).to_equal('2345');
   end;
 
   procedure zero_start_position is
