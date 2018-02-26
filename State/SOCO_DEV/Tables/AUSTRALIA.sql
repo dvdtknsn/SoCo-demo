4a9dfbe2c0e69a9742fa8be12bdbe6139df819fb
dvdtknsn <David.Atkinson@red-gate.com>
2018-02-26 11:20:52 +0000

    Some changes

---
+++ State/SOCO_DEV/Tables/AUSTRALIA.sql
@@ -0,0 +1,3 @@
+CREATE TABLE australia (
+  column1 VARCHAR2(20 BYTE)
+);
--- State/SOCO_DEV/Tables/DEMOTABLE.sql
+++ State/SOCO_DEV/Tables/DEMOTABLE.sql
@@ -2,5 +2,6 @@ CREATE TABLE demotable (
   column1 VARCHAR2(20 BYTE),
   column2 VARCHAR2(20 BYTE),
   column3 VARCHAR2(20 BYTE),
-  column4 VARCHAR2(20 BYTE)
+  column4 VARCHAR2(20 BYTE),
+  column5 VARCHAR2(20 BYTE)
 );
