92809b134b7e17d5adb0de2f467a1a97d7878915
dvdtknsn <David.Atkinson@red-gate.com>
2018-02-26 11:40:15 +0000

    I'm reverting to known good

--- State/SOCO_DEV/Tables/AUSTRALIA.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE australia (
-  column1 VARCHAR2(20 BYTE)
-);
--- State/SOCO_DEV/Tables/DEMOTABLE.sql
+++ State/SOCO_DEV/Tables/DEMOTABLE.sql
@@ -2,6 +2,5 @@ CREATE TABLE demotable (
   column1 VARCHAR2(20 BYTE),
   column2 VARCHAR2(20 BYTE),
   column3 VARCHAR2(20 BYTE),
-  column4 VARCHAR2(20 BYTE),
-  column5 VARCHAR2(20 BYTE)
+  column4 VARCHAR2(20 BYTE)
 );
